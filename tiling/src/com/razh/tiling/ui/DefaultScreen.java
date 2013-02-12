3aec84703cba93576936818a7ba0868e9f12d10d
Raymond Zhou <somecrazyidiot@gmail.com>
2013-02-11 19:16:59 -0500

    Start work on various Screens.

---
+++ tiling/src/com/razh/tiling/ui/DefaultScreen.java
@@ -0,0 +1,7 @@
+package com.razh.tiling.ui;
+
+import com.badlogic.gdx.Screen;
+
+public abstract class DefaultScreen implements Screen {
+
+}
---
+++ tiling/src/com/razh/tiling/ui/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.razh.tiling.ui;
+
+public class SplashScreen extends DefaultScreen {
+
+	@Override
+	public void render(float delta) {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
